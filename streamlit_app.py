1b2ded83514c9d5a56c1e8e397e232cb5424a946
Baiochi <jao.baiochi@gmail.com>
2022-09-28 21:49:45 -0300

    file upload

---
+++ streamlit_app.py
@@ -0,0 +1,119 @@
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+import joblib
+
+from utils import select_model, compute_metrics
+
+from sklearn import set_config
+set_config(display="diagram")
+
+LABELS = np.array(['educação', 'finanças', 'indústrias', 'orgão público', 'varejo'])
+df = pd.read_csv('data/dataset.csv')
+
+st.set_page_config(
+    page_title="Hand Talk Challenge",
+    page_icon="style/favicon.png",
+)
+
+if 'model' not in st.session_state:
+    st.session_state['model'] = False
+
+st.title('Multi-label Classification of Phrases')
+st.header('Hand Talk Challenge')
+st.markdown(
+	'''
+	Write your own sentence and see how the model classifies it.
+	You can also select different models and see how they perform.
+	'''
+)
+
+# --------------------------------------------------------------------
+# 								Sidebar		
+# --------------------------------------------------------------------
+
+model_selected = st.sidebar.selectbox(
+	label="Select the model:", 
+	options=[
+		# "Decision Tree",
+		# "Gradient Boosting",
+		# "Logistic Regression",
+		"Multinomial Naive Bayes", 
+		# "Random Forest", 
+		# "Support Vector Machine",
+		])
+
+st.session_state["model"] = select_model(model_selected)
+
+model_metrics = compute_metrics(df, st.session_state["model"])
+# metrics_df, train_roc_auc_fig, test_roc_auc_fig,
+# clf_report_train, clf_report_test
+
+# ---------------------------------------------------------------
+
+st.sidebar.write('Pipeline diagram: ')
+st.sidebar.write(st.session_state['model'][1])
+
+# --------------------------------------------------------------------
+# 							Main Section		
+# --------------------------------------------------------------------
+
+# Given a input sentence, predict the labels
+with st.form(key='my_form'):
+	text_input = st.text_input(label="Write your sentence here:")
+	submit_button = st.form_submit_button(label='Predict')
+
+	if submit_button:
+		# Labels predicted
+		labels_pred = st.session_state["model"].predict([text_input])[0].astype(bool)
+		labels_pred = LABELS[labels_pred]
+		if labels_pred.size==0:
+			st.write("No labels predicted.")
+		else:
+			st.write(f'Predicted labels: {labels_pred}')
+		
+		# Labels probabilities
+		pred_proba = st.session_state["model"].predict_proba([text_input])
+
+		if len(pred_proba) == 1:	# ClassifierChains case
+			pred_proba_df = pd.DataFrame({
+				'Category': LABELS,
+				'Probability': [str(round(proba*100,2)) + ' %' for proba in pred_proba[0]]
+			})
+		else:		# Other cases
+			pred_proba_df = pd.DataFrame({
+				'Category': LABELS,
+				'Probability': [str(round(proba[0][1]*100,2)) + ' %' for proba in pred_proba]
+			})
+		st.write('Predicted probabilities:')
+		st.dataframe(pred_proba_df)
+
+# View data frame
+if st.checkbox('Preview data frame', value=False, key='df_checkbox'):
+	st.dataframe(df)
+
+# Classification Report
+if st.checkbox('Show Classification Report', value=False, key='clf_report_checkbox'):
+	col1, col2 = st.columns(2)
+	with col1:
+		st.subheader('Train dataset')
+		st.dataframe(model_metrics['clf_report_train'])
+	with col2:
+		st.subheader('Test dataset')
+		st.dataframe(model_metrics['clf_report_test'])
+
+# ROC AUC
+if st.checkbox('Show ROC AUC', value=False, key='roc_auc_checkbox'):
+	col1, col2 = st.columns(2)
+	with col1:
+		st.subheader('Train dataset')
+		st.plotly_chart(model_metrics['train_roc_auc_fig'])
+	with col2:
+		st.subheader('Test dataset')
+		st.plotly_chart(model_metrics['test_roc_auc_fig'])
+
+st.markdown('author: [@baiochi](http://github.com/baiochi)')
+
+
+
---
+++ utils.py
@@ -0,0 +1,181 @@
+
+# Utils for computing models in the Streamlit app
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+import joblib
+import plotly.graph_objects as go
+import plotly.express as px
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import hamming_loss, auc, roc_curve, roc_auc_score, f1_score, classification_report
+
+
+# Compute metrics for multiclass ROC AUC
+def multiclass_rouc_auc(y_true, y_score, n_classes):
+    
+    # Get FP/TP and ROC AUC Score for each class
+    fpr = dict(); tpr = dict(); roc_auc = dict();
+
+    for i in range(y_score.shape[1]):
+        # false positives and true positives
+        fpr[i], tpr[i], _ = roc_curve(y_true[:, i], y_score[:, i])
+        # roc auc score
+        roc_auc[i] = roc_auc_score(y_true[:, i], y_score[:, i])
+    
+    # Compute micro-average ROC curve and ROC area from prediction scores
+    fpr['micro'], tpr['micro'], _ = roc_curve(y_true.ravel(), y_score.ravel())
+    roc_auc['micro'] = auc(fpr['micro'], tpr['micro'])
+    
+    # Aggregate all false positive rates
+    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+    
+    # Interpolate all ROC curves at this points
+    mean_tpr = np.zeros_like(all_fpr)
+    for i in range(n_classes):
+        mean_tpr += np.interp(all_fpr, fpr[i], tpr[i])
+    
+    # Average it and compute AUC
+    mean_tpr /= n_classes
+    fpr['macro'] = all_fpr
+    tpr['macro'] = mean_tpr
+    roc_auc['macro'] = auc(fpr['macro'], tpr['macro'])
+    
+    return fpr, tpr, roc_auc
+
+# Create figure of multiclass ROC AUC
+def plot_multiclass_roc_auc(fpr, tpr, roc_auc, labels, title=None):
+    
+    fig = go.Figure()
+
+    # Baseline roc curve
+    fig.add_shape(type='line', line=dict(dash='dash'),
+                x0=0, x1=1, y0=0, y1=1)
+    # Plot curve for each clas
+    for index, label in enumerate(labels):
+        name = f'{label} (area = {roc_auc[index]:.3f})'
+        fig.add_trace(go.Scatter(x=fpr[index], y=tpr[index], name=name, mode='lines'))
+
+    # Plot micro and macro average
+    fig.add_trace(go.Scatter(x=fpr['macro'], y=tpr['macro'], name=f'macro-average (area = {roc_auc["macro"]:.2f})', mode='lines', line_dash='dot'))
+    fig.add_trace(go.Scatter(x=fpr['micro'], y=tpr['micro'], name=f'micro-average (area = {roc_auc["micro"]:.2f})', mode='lines', line_dash='dot'))
+
+    fig.update_layout(
+        title_text = title + ' ROC curves',
+        xaxis_title='False Positive Rate',
+        yaxis_title='True Positive Rate',
+        yaxis=dict(scaleanchor='x', scaleratio=1),
+        xaxis=dict(constrain='domain'),
+        legend=dict(
+            yanchor="bottom",
+            y=0.02,
+            xanchor="left",
+            x=0.55
+        ),
+        autosize=False,
+        width=600, height=500,
+    )
+    
+    return fig
+
+
+
+def compute_metrics(df, model):
+
+	# Read data, set features and targets
+	X = df['sentence']
+	y = df['category']
+
+	# Binarize labels
+	mlb = MultiLabelBinarizer()
+	y = mlb.fit_transform(df['category'].apply(str.split, sep=','))
+	labels = mlb.classes_
+
+	# Train test split
+	X_train, X_test, y_train, y_test = train_test_split(
+			X, y, test_size=0.2, random_state=42)
+
+	# Predictions
+	y_pred_train = model.predict(X_train)
+	y_pred_test = model.predict(X_test)
+	y_proba_train = model.predict_proba(X_train)
+	y_proba_test = model.predict_proba(X_test)
+
+	# Acc score
+	avr_acc_train = model.score(X_train, y_train)*100
+	avr_acc_test = model.score(X_test, y_test)*100
+	
+	# Hamming Loss
+	hamming_loss_train = hamming_loss(y_train, model.predict(X_train))
+	hamming_loss_test = hamming_loss(y_test, model.predict(X_test))
+
+	# Cross validation
+	cv_score = cross_val_score(model, X, y, cv=5, scoring='accuracy').mean()*100
+
+	# ROC AUC
+	fpr_train, trp_train, roc_auc_train = multiclass_rouc_auc(y_train, y_proba_train, len(labels))
+	fpr_test, trp_test, roc_auc_test = multiclass_rouc_auc(y_test, y_proba_test, len(labels))
+	# ROC AUC figure
+	train_roc_auc_fig = plot_multiclass_roc_auc(fpr_train, trp_train, roc_auc_train, labels, title='Train dataset')
+	test_roc_auc_fig = plot_multiclass_roc_auc(fpr_test, trp_test, roc_auc_test, labels, title='Test dataset')
+
+	# Compute precision, recall and f1 score for each label, plus micro and macro average
+	clf_report_train = pd.DataFrame(classification_report(y_train, y_pred_train, output_dict=True)).T
+	clf_report_test = pd.DataFrame(classification_report(y_test, y_pred_test, output_dict=True)).T
+
+	# Rename axis with labels
+	clf_report_train.index = list(labels) + ['micro avg', 'macro avg', 'weighted avg', 'samples avg']
+	clf_report_test.index = list(labels) + ['micro avg', 'macro avg', 'weighted avg', 'samples avg']
+
+	# Add the ROC AUC scores computed previously
+	clf_report_train['roc auc'] = list(roc_auc_train.values()) + ([np.nan] * 2)
+	clf_report_test['roc auc'] = list(roc_auc_test.values()) + ([np.nan] * 2)
+	
+	metrics_df = pd.DataFrame({
+		'Train': [avr_acc_train, hamming_loss_train],
+		'Test': [avr_acc_test, hamming_loss_test],
+	},index=['Accuracy', 'Hamming Loss'])
+	print(f'Cross Validation Score: {cv_score:.2f} %')
+
+	return {
+		'metrics_df': metrics_df,
+		'train_roc_auc_fig': train_roc_auc_fig,
+		'test_roc_auc_fig': test_roc_auc_fig,
+		'clf_report_train': clf_report_train,
+		'clf_report_test': clf_report_test,
+	}
+
+# Streamlit render: select model from list
+def select_model(model_selected):
+
+	if model_selected == "Decision Tree":
+		model = joblib.load('models/decision_tree.joblib')
+
+	elif model_selected=="Gradient Boosting":
+		model = joblib.load('models/gradient_boosting.joblib')
+
+	elif model_selected=="Logistic Regression":
+		model = joblib.load('models/logistic_regression.joblib')
+
+	elif model_selected=="Multinomial Naive Bayes":
+		multiclass_strategy = st.sidebar.selectbox(
+			label="Multiclass strategy:",
+			options=['Binary Relevance', 'Classifier Chain', 'Label Powerset'])
+
+		# if multiclass_strategy=="Binary Relevance":
+		# 	model = joblib.load('models/multinomial_nb_moc.joblib')
+
+		if multiclass_strategy=="Classifier Chain":
+			model = joblib.load('models/multinomial_nb_cc.joblib')
+
+		elif multiclass_strategy=="Label Powerset":
+			model = joblib.load('models/multinomial_nb_lp.joblib')	
+
+	elif model_selected=="Random Forest":
+		model = joblib.load('models/random_forest.joblib')
+
+	elif model_selected=="Support Vector Machine":
+		model = joblib.load('models/svm.joblib')
+	
+	return model
